e86c982625663a8b48ef06b87713ed85677df652
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-03-03 16:49:31 +0100

    Make appNavigator use react-navigation

--- js/appNavigator.js
+++ js/appNavigator.js
@@ -1,85 +1,38 @@
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {StackNavigator, TabNavigator} from 'react-navigation';
 
-import React, { Component } from 'react';
-import { BackAndroid, StatusBar, NavigationExperimental } from 'react-native';
-import { connect } from 'react-redux';
-import { actions } from 'react-native-navigation-redux-helpers';
-import Home from './components/home/';
+import DeveloperScreen from './components/developer';
+import HomeScreen from './components/home';
+import SampleScreen from './components/sample';
 
-
-import Dev from './components/dev/';
-import Login from './components/login/';
-
-
-const {
-  popRoute,
-} = actions;
-
-const {
-  CardStack: NavigationCardStack,
-} = NavigationExperimental;
-
-class AppNavigator extends Component {
-
-  static propTypes = {
-    popRoute: React.PropTypes.func,
-    navigation: React.PropTypes.shape({
-      key: React.PropTypes.string,
-      routes: React.PropTypes.array,
-    }),
-  }
-
-  componentDidMount() {
-    BackAndroid.addEventListener('hardwareBackPress', () => {
-      const routes = this.props.navigation.routes;
-
-      if (routes[routes.length - 1].key === 'home') {
-        return false;
-      }
-
-      this.props.popRoute(this.props.navigation.key);
-      return true;
-    });
-  }
-
-
-  popRoute() {
-    this.props.popRoute();
-  }
-
-  _renderScene(props) { // eslint-disable-line class-methods-use-this
-    switch (props.scene.route.key) {
-      case 'home':
-        return <Home />;
-     case 'dev':
-        return <Dev />;
-      case 'login':
-        return <Login />;
-      default :
-        return <Home />;
-    }
-  }
-
-  render() {
-
-    return (
-        <NavigationCardStack
-          navigationState={this.props.navigation}
-          renderOverlay={this._renderOverlay}
-          renderScene={this._renderScene}
-        />
-
-    );
-  }
-}
-
-function bindAction(dispatch) {
-  return {
-    popRoute: key => dispatch(popRoute(key)),
-  };
-}
-
-const mapStateToProps = state => ({
-  navigation: state.cardNavigation,
+const HomeTab = StackNavigator({
+	HomeScreen: {
+		screen: HomeScreen
+	}
 });
 
-export default connect(mapStateToProps, bindAction)(AppNavigator);
+const DeveloperTab = StackNavigator({
+	DeveloperScreen: {
+		screen: DeveloperScreen
+	},
+	SampleScreen: {
+		screen: SampleScreen
+	}
+});
+
+const AppNavigator = TabNavigator({
+	HomeTab: {
+		screen: HomeTab
+	},
+	DeveloperTab: {
+		screen: DeveloperTab
+	}
+}, {
+	tabBarPosition: 'bottom',
+	tabBarOptions: {
+		activeTintColor: '#e91e63'
+	}
+});
+
+export default AppNavigator;
