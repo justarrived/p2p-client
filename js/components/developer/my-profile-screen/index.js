09b9e6fc502489b69c4329c34a81be9bf4c486fe
bassel9100 <bassel91@Bassels-MacBook-Pro.local>
2017-03-10 14:44:25 +0100

    Move makepaymentscreen to a new screen myprofile

---
+++ js/components/developer/my-profile-screen/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+import { Container, Tab, Tabs, Text } from 'native-base';
+import MakePaymentScreen from '../make-payment-screen';
+
+export default class MyProfileScreen extends Component {
+  static navigationOptions = {
+    tabBar: {
+      label: 'Developer',
+    },
+    title: 'My Profile',
+  };
+  render() {
+    return (
+      <Container>
+        <Tabs>
+          <Tab heading="PERSONUPPGIFTER" >
+            <Text> hej </Text>
+          </Tab>
+          <Tab heading="BETALUPPGIFTER">
+            <MakePaymentScreen />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
