45c6aff95d745fe3bf4861c32ef3173556845806
Bassel Kanaan <bassel.9100@gmail.com>
2017-03-12 01:36:51 +0100

    Alert notification when you press either two credit cards

--- js/components/developer/payment-info-screen/index.js
+++ js/components/developer/payment-info-screen/index.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text, Col, Grid, ListItem, Form, Input, Item, CheckBox, Content, Button, Body, Label } from 'native-base';
+import { View, Alert } from 'react-native';
+import { Text, ListItem, Thumbnail, Body, Left, Form, Content, Button } from 'native-base';
+
+const alertMessage1 = 'För Kortet .... .... .... 4499\n';
+const alertMessage2 = 'För Kortet .... .... .... 3232\n';
+
 
 export default class MakePaymentScreen extends Component {
+  pressCreditCard = () => {
+    alert(this.props.title);
+  }
+
   render() {
     return (
       <Content>
         <Form>
-          <ListItem>
-            <Text>Kort 1</Text>
+          <ListItem avatar onPress={() => Alert.alert('Bekräfta Betalning', alertMessage1)}>
+            <Left>
+              <Thumbnail source={require('./master.png')} />
+            </Left>
+            <Body>
+              <Text> .... .... .... 4499</Text>
+              <Text note>Master Card</Text>
+            </Body>
           </ListItem>
-          <ListItem>
-            <Text>Kort 2</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Kort 3</Text>
+          <ListItem avatar onPress={() => Alert.alert('Bekräfta Betalning', alertMessage2)}>
+            <Left>
+              <Thumbnail source={require('./visa.png')} />
+            </Left>
+            <Body>
+              <Text> .... .... .... 3232</Text>
+              <Text note>Visa Card</Text>
+            </Body>
           </ListItem>
         </Form>
         <View style={{ padding: 20 }}>
