27cb70b5b80a6f2e6272c74c61d565fd5cc5b2ab
Carl Jansson <carjan91@gmail.com>
2017-03-21 10:39:07 +0100

    I18n moved into folder structure

--- js/components/developer/sample-screen/index.js
+++ js/components/developer/sample-screen/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import I18n from '../../../I18n';
+import I18n from '../../../../I18n/components/developer/sample-screen/I18n';
 
 export default class SampleScreen extends Component {
   static navigationOptions = {
     tabBar: {
       label: I18n.t('dev'),
     },
-    title: 'Sample Screen',
+    title: I18n.t('screenTitle'),
   };
 
   render() {
