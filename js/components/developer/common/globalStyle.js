931f7b29a6d8e699f9eadc0b87ed51553309941e
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-03-21 23:04:21 +0100

    Create place card preview component

--- js/components/developer/common/globalStyle.js
+++ js/components/developer/common/globalStyle.js
@@ -1,10 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const PADDING = 10;
+const ROW_MARGIN = 10;
 
 const globalStyle = StyleSheet.create({
   padder: {
     padding: PADDING,
+  },
+
+  rowMargin: {
+    marginTop: ROW_MARGIN,
   },
 });
 
