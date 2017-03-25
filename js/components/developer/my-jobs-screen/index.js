032ec85b7c76f6bbe5c2c3e1c90eda6d4c3b0cad
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-03-25 16:55:27 +0100

    Use ListView to display jobs in MyJobsScreen

--- js/components/developer/my-jobs-screen/index.js
+++ js/components/developer/my-jobs-screen/index.js
@@ -4,28 +4,23 @@ import MyJobsTab from './myJobsTab';
 import { JOB_STATUS } from '../../common/constants';
 
 // Temporary example data for active jobs.
-const activeJobsData = [
-  { title: 'Pågående uppdrag',
-    jobs: [
-      { title: 'Snöskottning', date: '14 April 20:00', status: JOB_STATUS.ACTIVE },
-      { title: 'Gräsklippning', date: '11 Maj 10:00', status: JOB_STATUS.ACTIVE },
-    ],
-  },
-  { title: 'Ej tilldelade uppdrag',
-    jobs: [
-      { title: 'Snöskottning', date: '2 December 19:00', status: JOB_STATUS.UNASSIGNED },
-    ],
-  },
-];
+const activeJobsData = {
+  'Pågående uppdrag': [
+    { title: 'Snöskottning', date: '14 April 20:00', status: JOB_STATUS.ACTIVE },
+    { title: 'Gräsklippning', date: '11 Maj 10:00', status: JOB_STATUS.ACTIVE },
+  ],
+  'Ej tilldelade uppdrag': [
+    { title: 'Snöskottning', date: '2 December 19:00', status: JOB_STATUS.UNASSIGNED },
+  ],
+};
 
 // Temporary example data for archived jobs.
-const archivedJobsData = [
-  { title: 'Avslutade uppdrag',
-    jobs: [
-      { title: 'Snöskottning', date: '14 Januari 18:00', status: JOB_STATUS.FINISHED },
-    ],
-  },
-];
+const archivedJobsData = {
+  'Avslutade uppdrag': [
+    { title: 'Snöskottning', date: '14 Januari 18:00', status: JOB_STATUS.FINISHED },
+    { title: 'Snöskottning', date: '14 Januari 18:00', status: JOB_STATUS.FINISHED },
+  ],
+};
 
 export default class MyJobsScreen extends Component {
   static navigationOptions = {
