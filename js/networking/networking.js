50658048fe6acc8e7d18d454e5250006248c6141
Carl Jansson <carjan91@gmail.com>
2017-03-28 15:57:16 +0200

    Initial networking class used by webAppJobs example

---
+++ js/networking/networking.js
@@ -0,0 +1,48 @@
+/*
+Class containing basic networking methods
+*/
+
+/* global fetch b:true*/
+
+// GET url
+export function get(url, handleResponse, onSuccess, onError) {
+  fetch(url)
+    .then(response => handleResponse(response))
+    .then(responseJson => onSuccess(responseJson))
+    .catch(error => onError(error))
+    .done();
+}
+
+// GET json data from url
+export function getJson(url, onSuccess, onError) {
+  get(url,
+    (response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      throw new Error('Response was not 200 OK');
+    },
+    responseJson => onSuccess(responseJson),
+    error => onError(error));
+}
+
+// POST JSON data to url and handle JSON response
+export function postJson(url, json, onSuccess, onError) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(json),
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+    throw new Error('Response was not 200 OK');
+  })
+  .then(responseJson => onSuccess(responseJson))
+  .catch(error => onError(error))
+  .done();
+}
