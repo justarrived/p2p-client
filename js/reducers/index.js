f15c2dad84f924baac690afbdb7dd43fd8ff564b
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-04-03 23:00:34 +0200

    Add jobTypes reducer in combineReducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,7 +1,8 @@
 import account from './account';
 import person from './person';
 import languages from './languages';
+import jobTypes from './jobTypes';
 
 const { combineReducers } = require('redux');
 
-export default combineReducers({ person, languages, account });
+export default combineReducers({ person, languages, account, jobTypes });
