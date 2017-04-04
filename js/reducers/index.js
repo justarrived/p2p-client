0deb09921fd17a6dac967a47ed2dee31fa85028e
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-04-04 16:17:25 +0200

    Add ownedJobs reducer in combineReducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,8 +2,9 @@ import account from './account';
 import person from './person';
 import languages from './languages';
 import jobTypes from './jobTypes';
+import ownedJobs from './ownedJobs';
 import creditCards from './creditCards';
 
 const { combineReducers } = require('redux');
 
-export default combineReducers({ person, languages, account, jobTypes, creditCards });
+export default combineReducers({ person, languages, account, jobTypes, ownedJobs, creditCards });
