1f5307cc47b50ff61129d11d70af8d81ab5ab2fe
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-04-04 13:36:00 +0200

    Make ChooseLanguageScreen use data from Redux

--- js/components/developer/choose-language-screen/index.js
+++ js/components/developer/choose-language-screen/index.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Card, Thumbnail, Container, Content, List } from 'native-base';
+import { connect } from 'react-redux';
 import chooseLanguageStyles from './chooseLanguageStyles';
 import GlobalStyle from '../../common/globalStyle';
 import CardHeader from '../../common/card-header/cardHeader';
 import SingleRowListItem from '../../common/single-row-list-item/singleRowListItem';
 
+// TODO Replace temporary placeholder image
 const LOGO_URL = 'https://facebook.github.io/react/img/logo_og.png';
 
-// Temporary data. Will be handled in another way in the future.
-const LANGUAGES = ['Svenska', 'English', 'Español', 'Dansk', 'Norsk'];
-
-export default class ChooseLanguageScreen extends Component {
+class ChooseLanguageScreen extends Component {
   static navigationOptions = {
     title: 'Choose Language',
   };
 
-  renderRow = language => <SingleRowListItem text={language} icon="arrow-forward" />
+  // TODO Improve typechecking
+  static propTypes = {
+    languages: PropTypes.objectOf(PropTypes.any).isRequired,
+  };
+
+  renderRow = lang => <SingleRowListItem text={lang.attributes.local_name} icon="arrow-forward" />
 
   render() {
     return (
@@ -30,10 +34,14 @@ export default class ChooseLanguageScreen extends Component {
           </View>
           <Card>
             <CardHeader title="Välj språk" subtitle="Språket som ska visas i appen" icon="globe" />
-            <List dataArray={LANGUAGES} renderRow={this.renderRow} />
+            <List dataArray={this.props.languages.languages.data} renderRow={this.renderRow} />
           </Card>
         </Content>
       </Container>
     );
   }
 }
+
+const mapStateToProps = state => ({ languages: state.languages });
+
+export default connect(mapStateToProps)(ChooseLanguageScreen);
