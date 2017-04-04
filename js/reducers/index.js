e368d8b20385b779735a012282c26051b23c8c01
Christoffer Karlsson <chriskar@student.chalmers.se>
2017-04-04 14:26:08 +0200

    Add creditCards reducer in combineReducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,7 +2,8 @@ import account from './account';
 import person from './person';
 import languages from './languages';
 import jobTypes from './jobTypes';
+import creditCards from './creditCards';
 
 const { combineReducers } = require('redux');
 
-export default combineReducers({ person, languages, account, jobTypes });
+export default combineReducers({ person, languages, account, jobTypes, creditCards });
