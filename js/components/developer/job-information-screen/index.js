33be4556a46d9d133a2e1db2dffc0396a6ff6b10
Billthekid <ingvarsson.anton@gmail.com>
2017-04-07 10:27:21 +0200

    remove blank lines

--- js/components/developer/job-information-screen/index.js
+++ js/components/developer/job-information-screen/index.js
@@ -3,7 +3,6 @@ import { Container, Tab, Tabs } from 'native-base';
 import JobPreviewScreen from '../job-preview-screen';
 import ChooseWorkerScreen from '../choose-worker-screen';
 
-
 export default class JobsInformationScreen extends Component {
   static navigationOptions = {
     title: 'Mina Uppdrag',
