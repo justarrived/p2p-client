27c9671b6cee05397ff015185a71529016e83cf3
Carl Jansson <carjan91@gmail.com>
2017-04-20 20:41:40 +0200

    Initial redux and networking methods relating to jobs

---
+++ js/networking/jobs.js
@@ -0,0 +1,54 @@
+import { getAuthJson, patchAuthJson, postJson } from './networking';
+import BASE_URL from './url';
+/*
+Class with methods for manipulating user info against API.
+*/
+
+const JOBS_PATH = `${BASE_URL}/api/v1/jobs`;
+
+function getJobPath(jobId) {
+  return `${JOBS_PATH}/${jobId}`;
+}
+
+function getJobUserPath(jobId, userId) {
+  return `${getJobPath(jobId)}/users/${userId}`;
+}
+
+// TODO test
+// get a specific job
+export function getJob(jobId, token, onSuccess, onError) {
+  getAuthJson(getJobPath(jobId), token, onSuccess, onError);
+}
+
+// TODO test
+// Get all the jobs
+export function getJobs(token, onSuccess, onError) {
+  getAuthJson(JOBS_PATH, token, onSuccess, onError);
+}
+
+// TODO test
+export function getJobsUser(jobId, userId, token, onSuccess, onError) {
+  getAuthJson(`${getJobUserPath(jobId, userId)}/job-user`, token, onSuccess, onError);
+}
+
+// TODO test
+export function getJobsUserMissingTraits(jobId, userId, token, onSuccess, onError) {
+  getAuthJson(`${getJobUserPath(jobId, userId)}/missing-traits`, token, onSuccess, onError);
+}
+
+// TODO test
+export function getJobsUsersMatching(jobId, token, onSuccess, onError) {
+  getAuthJson(`${getJobPath(jobId)}/matching_users`, token, onSuccess, onError);
+}
+
+// TODO test
+// Update a job
+export function patchJob(jobId, token, json, onSuccess, onError) {
+  patchAuthJson(getJobPath(jobId), token, json, onSuccess, onError);
+}
+
+// TODO test
+// Post a new job
+export function postJob(json, onSuccess, onError) {
+  postJson(JOBS_PATH, json, onSuccess, onError);
+}
---
+++ js/reducers/jobs.js
@@ -0,0 +1,101 @@
+import { JOB_REQUEST, JOB_RECEIVE, JOB_CREATE, JOB_UPDATE,
+  JOBS_REQUEST, JOBS_RECEIVE } from '../actions/jobs';
+import { SESSION_REMOVE } from '../actions/session';
+
+const initialState = {
+  jobId: null,
+  jobJson: null,
+  jobLoading: false,
+  error: null,
+};
+
+export default function (state = initialState, action) {
+  // console.log(`previous job state:\n${JSON.stringify(state, null, 4)}`);
+  if (action.type === JOB_REQUEST) {
+    // A request for a job has been sent
+    return {
+      ...state,
+      jobId: action.userId,
+      jobLoading: true,
+    };
+  }
+  if (action.type === JOBS_REQUEST) {
+    // A request for all jobs has been sent
+    return {
+      ...state,
+      jobId: null,
+      jobLoading: true,
+    };
+  }
+  if (action.type === JOB_RECEIVE) {
+    if (action.jobJson === null) {
+      // No data received. Save error
+      return {
+        ...state,
+        jobId: null,
+        jobLoading: false,
+        error: action.error,
+      };
+    }
+    // Receive and save a job object
+    let newJobJson;
+    if (state.jobJson === null) {
+      // If no other jobs exists in state
+      newJobJson = {
+        data: [action.jobJson.data],
+      };
+    } else {
+      // There are other jobs in state.
+      // First remove any old version of the job
+      state.jobJson.data.forEach(
+        (oldJob, index, oldArray) => {
+          if (oldJob.id === action.jobJson.data.id) {
+            oldArray.splice(index, 1);
+          }
+        },
+      );
+      // add new job
+      newJobJson = {
+        data: [...state.jobJson.data, action.jobJson.data],
+      };
+    }
+    return {
+      ...state,
+      jobId: action.userId,
+      jobJson: newJobJson,
+      jobLoading: false,
+      error: action.error,
+    };
+  }
+  if (action.type === JOBS_RECEIVE) {
+    // Receive and save all jobs
+    return {
+      ...state,
+      jobId: null,
+      jobJson: action.jobJson,
+      jobLoading: false,
+      error: action.error,
+    };
+  }
+  if (action.type === JOB_CREATE) {
+    // Request for job to be created has been sent
+    return {
+      ...state,
+      jobLoading: true,
+      error: null,
+    };
+  }
+  if (action.type === JOB_UPDATE) {
+    // Request for user to be updated has been sent
+    return {
+      ...state,
+      jobLoading: true,
+      error: null,
+    };
+  }
+  if (action.type === SESSION_REMOVE) {
+    // Remove local data when user signs out
+    return initialState;
+  }
+  return state;
+}
