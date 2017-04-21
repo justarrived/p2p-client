c73cd6620c5a7bca4405cb1239acba7ef214c47f
Tobias Tikka <tobias.tikka@gmail.com>
2017-04-21 13:41:12 +0200

    Update login screen
    Adapt design according to mockups, add JAButton

--- js/components/developer/login-screen/loginScreenStyles.js
+++ js/components/developer/login-screen/loginScreenStyles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { GRAY, DARK_GRAY } from '../../common/colors';
 
 const PADDING = 10;
 
@@ -7,24 +8,42 @@ const loginScreenStyles = StyleSheet.create({
     padding: PADDING,
   },
 
-  logoContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  loginHeader: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 120,
+    paddingBottom: 14,
   },
 
-  logo: {
-    width: 140,
-    height: 140,
-    borderRadius: 70,
-    alignItems: 'center',
-    margin: PADDING * 2,
+  inputFields: {
+    paddingTop:30,
+  },
+
+  loginDescription: {
+    fontSize: 14,
+    color: GRAY,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  inputField: {
+    fontSize: 14,
+    color: GRAY,
+    fontWeight: 'bold',
   },
 
   cardForm: {
     flex: 0,
-    paddingTop: PADDING,
-    paddingBottom: PADDING,
+    padding: 0,
+  },
+
+  forgotPassword: {
+    marginTop: 20,
+    textAlign: 'right',
+    color: DARK_GRAY,
+    fontSize: 12,
+    fontWeight: 'bold',
   },
 
   buttonContainer: {
@@ -33,13 +52,30 @@ const loginScreenStyles = StyleSheet.create({
     padding: PADDING,
   },
 
-  secondaryButtonsContainer: {
-    marginTop: PADDING * 2,
+  textBetweenButtons: {
+    textAlign: 'center',
+    fontSize: 10,
+    fontWeight: 'bold',
+    paddingTop: 15,
+    paddingBottom: 15,
+    color: GRAY,
   },
 
-  secondaryButtonSpacing: {
-    marginRight: PADDING,
+  footerStyling: {
+    backgroundColor: '#F0F0F0',
+    left: 0, 
+    right: 0,
+    bottom: 0,
+    padding: 12
   },
+
+  footerText: {
+    textAlign: 'center',
+    color: DARK_GRAY,
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+
 });
 
 module.exports = loginScreenStyles;
