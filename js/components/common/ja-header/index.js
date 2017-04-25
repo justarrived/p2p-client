7cfa1b44d4189bf59e87ae1fab813d236bf87029
Tobias Tikka <tobias.tikka@gmail.com>
2017-04-25 22:10:51 +0200

    Refactoring and adding JALogo to JAHeader

--- js/components/common/ja-header/index.js
+++ js/components/common/ja-header/index.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
+import JALogo from '../ja-logo';
 import styles from './style';
 
-{/*
-  Brand header used in several views.
-  */}
-class JAHeader extends Component {
+// Brand header used in several views
+const JAHeader = () => (
+  <View style={styles.container}>
+    <JALogo />
+    <Text style={styles.mainTitle}>
+      People
+    </Text>
+    <Text style={styles.subTitle}>
+      by Just Arrived
+    </Text>
+  </View>
+);
 
-  render = () => {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.mainTitle}>
-          People
-        </Text>
-        <Text style={styles.subTitle}>
-          by Just Arrived
-        </Text>
-      </View>
-    )
-  }
-}
 
 export default JAHeader;
