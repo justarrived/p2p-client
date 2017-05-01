2e35440ad6b908e2d1463369cee1b1a047e0ff35
antoningvarsson <ingvarsson.anton@gmail.com>
2017-05-01 17:39:10 +0200

    redesign of create-account-screen following mockups using new components

--- js/components/developer/create-account-screen/index.js
+++ js/components/developer/create-account-screen/index.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import { Container, Content, Form, Col, Row, Grid, Button, Card, CardItem, Text } from 'native-base';
+import { Container, Content, Col, Grid } from 'native-base';
+
+// styles
 import styles from './style';
+import GlobalStyle from '../../common/globalStyle';
+
 import I18n from '../../../i18n';
 import { requestCreateUser } from '../../../actions/user';
 import { createJsonDataAttributes } from '../../../networking/json';
 import {
-  changeFirstName, changeLastName, changeAddress, changePostCode, changePostArea,
-  changePhoneNumber, changeEmail, changePassword, toggleCheckBox,
+  changeFirstName, changeLastName, changeEmail, changePassword, toggleCheckBox, changeAppLanguage,
 } from '../../../actions/account';
 
-import EmailInput from '../../common/email-input';
-import PhoneInput from '../../common/numeric-input';
-import PasswordInput from '../../common/password-input';
-import TextInput from '../../common/text-input';
-import PostcodeInput from '../../common/post-code-input';
+import JATagline from '../../common/ja-tagline';
+import JAButton from '../../common/ja-button';
+import { JA_BUTTON, JA_INPUT } from '../../common/constants';
+import JAInput from '../../common/ja-input';
 import CheckBoxPUL from '../../common/checkbox-with-text';
-import GlobalStyle from '../../common/globalStyle';
+import LanguageDropdown from '../../common/language-dropdown/languageDropdown';
+import Footer from '../../common/footer/footer';
+
 
 // TODO Replace these standard values with real values.
 // These are used during development only.
+// LanguageDropdown shows one language when first rendered even though
+// no language is seleceted. Could lead to bugs
 function getCreateUserJson(email, password) {
   return createJsonDataAttributes({
     email,
@@ -33,34 +39,24 @@ function getCreateUserJson(email, password) {
 }
 
 class CreateAccountScreen extends Component {
-  static navigationOptions = {
-    title: I18n.t('screen_titles.create_account'),
-  };
 
   static propTypes = {
-    changeFirstName: React.PropTypes.func.isRequired,
-    changeLastName: React.PropTypes.func.isRequired,
-    changeAddress: React.PropTypes.func.isRequired,
-    changePostCode: React.PropTypes.func.isRequired,
-    changePostArea: React.PropTypes.func.isRequired,
-    changePhoneNumber: React.PropTypes.func.isRequired,
-    changeEmail: React.PropTypes.func.isRequired,
-    changePassword: React.PropTypes.func.isRequired,
-    toggleCheckBox: React.PropTypes.func.isRequired,
-    account: React.PropTypes.shape({
+    changeFirstName: PropTypes.func.isRequired,
+    changeLastName: PropTypes.func.isRequired,
+    changeEmail: PropTypes.func.isRequired,
+    changePassword: PropTypes.func.isRequired,
+    changeAppLanguage: PropTypes.func.isRequired,
+    toggleCheckBox: PropTypes.func.isRequired,
+    account: PropTypes.shape({
       firstName: React.PropTypes.string,
       lastName: React.PropTypes.string,
-      address: React.PropTypes.string,
-      postCode: React.PropTypes.string,
-      postArea: React.PropTypes.string,
-      phoneNumber: React.PropTypes.string,
       email: React.PropTypes.string,
       password: React.PropTypes.string,
+      appLanguage: React.PropTypes.string,
       userAgreement: React.PropTypes.bool,
-      disabled: React.PropTypes.boolean,
     }).isRequired,
-    goToLogin: React.PropTypes.func.isRequired,
-    signUp: React.PropTypes.func.isRequired,
+    goToLogin: PropTypes.func.isRequired,
+    signUp: PropTypes.func.isRequired,
   }
 
   createAccount() {
@@ -69,92 +65,50 @@ class CreateAccountScreen extends Component {
 
   render() {
     return (
-      <Container>
-        <Content contentContainerStyle={GlobalStyle.padder}>
-          <Card>
-            <CardItem bordered style={StyleSheet.flatten(GlobalStyle.noPadding)}>
-              <Form style={StyleSheet.flatten(styles.form)}>
-                <Grid>
-                  <Row>
-                    <Col>
-                      <TextInput
-                        title={I18n.t('account.first_name')}
-                        onChange={input => this.props.changeFirstName(input)}
-                      />
-                    </Col>
-                    <Col>
-                      <TextInput
-                        title={I18n.t('account.last_name')}
-                        onChange={input => this.props.changeLastName(input)}
-                      />
-                    </Col>
-                  </Row>
-                  <TextInput
-                    title={I18n.t('account.address')}
-                    onChange={input => this.props.changeAddress(input)}
-                  />
-                  <Row>
-                    <Col>
-                      <PostcodeInput
-                        title={I18n.t('account.postal_code')}
-                        onChange={input => this.props.changePostCode(input)}
-                      />
-                    </Col>
-                    <Col>
-                      <TextInput
-                        title={I18n.t('account.city')}
-                        onChange={input => this.props.changePostArea(input)}
-                      />
-                    </Col>
-                  </Row>
-                </Grid>
-                <PhoneInput
-                  title={I18n.t('account.phone_number')}
-                  onChange={input => this.props.changePhoneNumber(input)}
-                />
-                <EmailInput
-                  title={I18n.t('account.email')}
-                  onChange={input => this.props.changeEmail(input)}
-                />
-                <PasswordInput
-                  title={I18n.t('account.password')}
-                  onChange={input => this.props.changePassword(input)}
-                />
-                <CheckBoxPUL
-                  title={I18n.t('sign_up.accept_pul_and_terms')}
-                  isChecked={this.props.account.userAgreement}
-                  onClick={() => this.props.toggleCheckBox()}
-                />
-              </Form>
-            </CardItem>
-            <CardItem style={StyleSheet.flatten(GlobalStyle.noPadding)}>
-              <Grid style={StyleSheet.flatten(styles.buttonContainer)}>
-                <Row style={StyleSheet.flatten(styles.fullFlex)}>
-                  <Button
-                    block
-                    onPress={() => this.createAccount()}
-                    style={StyleSheet.flatten(styles.fullFlex)}
-                  >
-                    <Text style={StyleSheet.flatten(styles.regButtonText)}>
-                      {I18n.t('sign_up.sign_up_button')}
-                    </Text>
-                  </Button>
-                </Row>
-                <Row style={StyleSheet.flatten(styles.fullFlex)}>
-                  <Button
-                    small block bordered
-                    onPress={() => this.props.goToLogin()}
-                    style={StyleSheet.flatten(styles.secondButtonPadding)}
-                  >
-                    <Text>
-                      {I18n.t('sign_up.already_have_an_account_button')}
-                    </Text>
-                  </Button>
-                </Row>
-              </Grid>
-            </CardItem>
-          </Card>
+      <Container style={StyleSheet.flatten(GlobalStyle.whiteBackgroundColor)}>
+        <Content contentContainerStyle={GlobalStyle.modalPadder} >
+          <JATagline />
+          <Text style={styles.titleText}>{I18n.t('sign_up.create_account_title')}</Text>
+          <Grid>
+            <Col style={StyleSheet.flatten(styles.colPadding)}>
+              <JAInput
+                typeOfInput={JA_INPUT.FIRST_NAME}
+                onChange={text => this.props.changeFirstName(text)}
+              />
+            </Col>
+            <Col>
+              <JAInput
+                typeOfInput={JA_INPUT.LAST_NAME}
+                onChange={text => this.props.changeLastName(text)}
+              />
+            </Col>
+          </Grid>
+          <JAInput
+            typeOfInput={JA_INPUT.EMAIL}
+            onChange={text => this.props.changeEmail(text)}
+          />
+          <JAInput
+            typeOfInput={JA_INPUT.PASSWORD}
+            onChange={text => this.props.changePassword(text)}
+          />
+          <LanguageDropdown onChange={input => this.props.changeAppLanguage(input)} />
+          <CheckBoxPUL
+            title={I18n.t('sign_up.accept_pul_and_terms')}
+            isChecked={this.props.account.userAgreement}
+            onClick={() => this.props.toggleCheckBox()}
+          />
+          <View style={styles.buttonContainer}>
+            <JAButton
+              typeOfButton={JA_BUTTON.PRIMARY}
+              content={I18n.t('sign_up.sign_up_button')}
+              actionOnClick={() => this.createAccount()}
+            />
+          </View>
         </Content>
+        <Footer
+          onPress={() => this.props.goToLogin()}
+          title={I18n.t('sign_up.already_have_an_account_button')}
+        />
       </Container>
     );
   }
@@ -164,13 +118,10 @@ function bindAction(dispatch) {
   return {
     changeFirstName: input => dispatch(changeFirstName(input)),
     changeLastName: input => dispatch(changeLastName(input)),
-    changeAddress: input => dispatch(changeAddress(input)),
-    changePostCode: input => dispatch(changePostCode(input)),
-    changePostArea: input => dispatch(changePostArea(input)),
-    changePhoneNumber: input => dispatch(changePhoneNumber(input)),
     changeEmail: input => dispatch(changeEmail(input)),
     changePassword: input => dispatch(changePassword(input)),
     toggleCheckBox: () => dispatch(toggleCheckBox()),
+    changeAppLanguage: input => dispatch(changeAppLanguage(input)),
     signUp: (user, password) => dispatch(requestCreateUser(user, password)),
   };
 }
