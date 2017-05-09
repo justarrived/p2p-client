dd266116f9c4af06ccb6cf7461f66eaa9412a705
Carl Jansson <carjan91@gmail.com>
2017-05-09 10:32:09 +0200

    ProfileInputFields as a function

--- js/components/developer/my-profile-screen/profileInputFields.js
+++ js/components/developer/my-profile-screen/profileInputFields.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { Form, Col, Row, CardItem } from 'native-base';
@@ -10,67 +10,62 @@ import PostcodeInput from '../../common/post-code-input';
 import I18n from '../../../i18n';
 import { setAddress, setPostCode, setPostArea, setPhoneNumber, setEmail } from '../../../actions/userEdit';
 
-class ProfileInputFields extends Component {
-
-  static propTypes = {
-    setAddress: PropTypes.func.isRequired,
-    setPostCode: PropTypes.func.isRequired,
-    setPostArea: PropTypes.func.isRequired,
-    setPhoneNumber: PropTypes.func.isRequired,
-    setEmail: PropTypes.func.isRequired,
-    attributes: PropTypes.objectOf(PropTypes.any).isRequired,
-    editDisabled: PropTypes.bool.isRequired,
-  }
-
-  render() {
-    return (
-      <CardItem bordered style={StyleSheet.flatten(style.formContainer)}>
-        <Form style={StyleSheet.flatten(style.form)}>
+// Profile information content that can be edited
+const ProfileInputFields = props =>
+  <CardItem bordered style={StyleSheet.flatten(style.formContainer)}>
+    <Form style={StyleSheet.flatten(style.form)}>
+      <TextInput
+        title={I18n.t('account.address')}
+        onChange={input => props.setAddress(input)}
+        disabled={props.disabled}
+        defaultValue={props.attributes.street}
+      />
+      <Row>
+        <Col>
+          <PostcodeInput
+            title={I18n.t('account.postal_code')}
+            onChange={input => props.setPostCode(input)}
+            disabled={props.disabled}
+            defaultValue={props.attributes.zip}
+          />
+        </Col>
+        <Col>
           <TextInput
-            title={I18n.t('account.address')}
-            onChange={input => this.props.setAddress(input)}
-            disabled={this.props.editDisabled}
-            defaultValue={this.props.attributes.street}
+            title={I18n.t('account.city')}
+            onChange={input => props.setPostArea(input)}
+            disabled={props.disabled}
+            defaultValue={props.attributes.city}
           />
-          <Row>
-            <Col>
-              <PostcodeInput
-                title={I18n.t('account.postal_code')}
-                onChange={input => this.props.setPostCode(input)}
-                disabled={this.props.editDisabled}
-                defaultValue={this.props.attributes.zip}
-              />
-            </Col>
-            <Col>
-              <TextInput
-                title={I18n.t('account.city')}
-                onChange={input => this.props.setPostArea(input)}
-                disabled={this.props.editDisabled}
-                defaultValue={this.props.attributes.city}
-              />
-            </Col>
-          </Row>
-          <PhoneInput
-            title={I18n.t('account.phone_number')}
-            onChange={input => this.props.setPhoneNumber(input)}
-            disabled={this.props.editDisabled}
-            defaultValue={this.props.attributes.phone}
-          />
-          <EmailInput
-            title={I18n.t('account.email')}
-            onChange={input => this.props.setEmail(input)}
-            disabled={this.props.editDisabled}
-            defaultValue={this.props.attributes.email}
-          />
-        </Form>
-      </CardItem>
-    );
-  }
-}
+        </Col>
+      </Row>
+      <PhoneInput
+        title={I18n.t('account.phone_number')}
+        onChange={input => props.setPhoneNumber(input)}
+        disabled={props.disabled}
+        defaultValue={props.attributes.phone}
+      />
+      <EmailInput
+        title={I18n.t('account.email')}
+        onChange={input => props.setEmail(input)}
+        disabled={props.disabled}
+        defaultValue={props.attributes.email}
+      />
+    </Form>
+  </CardItem>;
+
+ProfileInputFields.propTypes = {
+  setAddress: PropTypes.func.isRequired,
+  setPostCode: PropTypes.func.isRequired,
+  setPostArea: PropTypes.func.isRequired,
+  setPhoneNumber: PropTypes.func.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  attributes: PropTypes.objectOf(PropTypes.any).isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
 
 const mapStateToProps = state => ({
   attributes: state.userEdit.attributes,
-  editDisabled: state.userEdit.disabled,
+  disabled: state.userEdit.disabled,
 });
 
 function bindAction(dispatch) {
